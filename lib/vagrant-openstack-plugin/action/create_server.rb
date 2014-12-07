04dddedcb0e6b8b61fd06412fd98a01077bdf0b3
mohitsethi <mohit@sethis.in>
2014-12-07 12:38:44 +0530

    increased timeout

--- lib/vagrant-openstack-plugin/action/create_server.rb
+++ lib/vagrant-openstack-plugin/action/create_server.rb
@@ -44,7 +44,7 @@ module VagrantPlugins
             :os_scheduler_hints => config.scheduler_hints,
             :availability_zone => config.availability_zone
           }
-          
+
           # Fallback to only one network, otherwise `config.networks` overrides
           unless config.networks
             if config.network
@@ -79,7 +79,7 @@ module VagrantPlugins
             end
             env[:ui].info("options[:nics]: #{options[:nics]}")
           end
-         
+
           # Output the settings we're going to use to the user
           env[:ui].info(I18n.t("vagrant_openstack.launching_server"))
           env[:ui].info(" -- Flavor: #{flavor.name}")
@@ -100,7 +100,7 @@ module VagrantPlugins
 
           # Wait for the server to finish building
           env[:ui].info(I18n.t("vagrant_openstack.waiting_for_build"))
-          retryable(:on => Fog::Errors::TimeoutError, :tries => 200) do
+          retryable(:on => Fog::Errors::TimeoutError, :tries => 2000) do
             # If we're interrupted don't worry about waiting
             next if env[:interrupted]
 
@@ -110,7 +110,7 @@ module VagrantPlugins
 
             # Wait for the server to be ready
             begin
-              server.wait_for(5) { ready? }
+              server.wait_for(500) { ready? }
               # Once the server is up and running assign a floating IP if we have one
               floating_ip = config.floating_ip
               # try to automatically associate a floating IP
@@ -134,50 +134,50 @@ module VagrantPlugins
                   end
                 end
               end
-                
+
               if floating_ip
                 env[:ui].info( "Using floating IP #{floating_ip}")
                 floater = env[:openstack_compute].addresses.find { |thisone| thisone.ip.eql? floating_ip }
                 floater.server = server
               end
-              
-              # Process disks if provided
-              volumes = Array.new
-              if config.disks && !config.disks.empty?
-                env[:ui].info(I18n.t("vagrant_openstack.creating_disks"))
-                config.disks.each do |disk|
-                  volume = env[:openstack_compute].volumes.all.find{|v| v.name ==
-                                                          disk["name"] and 
-                                                        v.description ==
-                                                          disk["description"] and
-                                                        v.size ==
-                                                          disk["size"] and
-                                                        v.ready? }
-                  if volume
-                    env[:ui].info("re-using volume: #{disk["name"]}")
-                    disk["volume_id"] = volume.id
-                  else
-                    env[:ui].info("creating volume: #{disk["name"]}")
-                    disk["volume_id"] = env[:openstack_compute].create_volume(
-                                         disk["name"], disk["description"], disk["size"]).\
-                                         data[:body]["volume"]["id"]
-                    volumes << { :id => disk["volume_id"] }
-                  end
 
-                  # mount points are not expected to be meaningful
-                  # add useful support if your cloud respects them
-                  begin
-                    server.attach_volume(disk["volume_id"], "/dev/vd#{("a".."z").to_a[server.volume_attachments.length + 1]}")
-                    server.wait_for{ volume_attachments.any?{|vol| vol["id"]==disk["volume_id"]} }
-                  rescue Excon::Errors::Error => e
-                    raise Errors::VolumeBadState, :volume => disk["name"], :state => e.message
-                  end
-                end
-              end
+              # Process disks if provided
+              # volumes = Array.new
+              # if config.has_key?("disks") and not config.disks.empty?
+              #   env[:ui].info(I18n.t("vagrant_openstack.creating_disks"))
+              #   config.disks.each do |disk|
+              #     volume = env[:openstack_compute].volumes.all.find{|v| v.name ==
+              #                                             disk["name"] and
+              #                                           v.description ==
+              #                                             disk["description"] and
+              #                                           v.size ==
+              #                                             disk["size"] and
+              #                                           v.ready? }
+              #     if volume
+              #       env[:ui].info("re-using volume: #{disk["name"]}")
+              #       disk["volume_id"] = volume.id
+              #     else
+              #       env[:ui].info("creating volume: #{disk["name"]}")
+              #       disk["volume_id"] = env[:openstack_compute].create_volume(
+              #                            disk["name"], disk["description"], disk["size"]).\
+              #                            data[:body]["volume"]["id"]
+              #       volumes << { :id => disk["volume_id"] }
+              #     end
+
+              #     # mount points are not expected to be meaningful
+              #     # add useful support if your cloud respects them
+              #     begin
+              #       server.attach_volume(disk["volume_id"], "/dev/vd#{("a".."z").to_a[server.volume_attachments.length + 1]}")
+              #       server.wait_for{ volume_attachments.any?{|vol| vol["id"]==disk["volume_id"]} }
+              #     rescue Excon::Errors::Error => e
+              #       raise Errors::VolumeBadState, :volume => disk["name"], :state => e.message
+              #     end
+              #   end
+              # end
 
               # store this so we can use it later
               env[:floating_ip] = floating_ip
-              
+
             rescue RuntimeError => e
               # If we don't have an error about a state transition, then
               # we just move on.
